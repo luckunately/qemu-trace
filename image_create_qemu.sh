13728658cabe4a923d844e5201cb07855bffb8ba
tom <fortily@student.ubc.ca>
2024-12-22 15:23:15 -0800

    make sure the server name is changed

--- image_create_qemu.sh
+++ image_create_qemu.sh
@@ -18,8 +18,10 @@ sudo mount -o loop,rw $IMGPATH $MOUNTPATH
 # sudo debootstrap focal $MOUNTPATH
 sudo debootstrap jammy $MOUNTPATH
 sudo chroot $MOUNTPATH apt-get update
-sudo chroot $MOUNTPATH apt-get install -y build-essential vim wget curl git gdb
+sudo chroot $MOUNTPATH apt-get install -y build-essential vim wget curl git gdb time
 # sudo chroot $MOUNTPATH apt-get install -y cgroup-tools # cgroupfs-mount cgroup-bin cgroup-lite cgroupfs-mount libcgroup1
-echo "set root password"
+echo "setup the server name to be qemu"
+echo "qemu" | sudo tee $MOUNTPATH/etc/hostname
+echo "set root password, you will login with root user"
 sudo chroot $MOUNTPATH passwd root
 sudo umount $MOUNTPATH``
